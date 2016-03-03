8e12314cf13a7aca0dcd3b6c7baf4d9cdcb310a2
5998 <m80005998@qq.com>
2016-03-03 22:20:12 +0400

    Modification Update Class 4
    Changed printing file destination with read and element detection location.

--- Client/src/client/Class4.java
+++ Client/src/client/Class4.java
@@ -6,35 +6,33 @@ import java.io.*;
 public class Class4 {
     public static void main(String args[]) {
         try {
-            String stringSearch = "230";
+            String stringSearch = "The Word Keys Goes Here";
             // Open the file c:\test.txt as a buffered reader
-            BufferedReader bf = new BufferedReader(
-            new FileReader(" ")
-            );
+            BufferedReader bf = new BufferedReader(new FileReader("Your Destination File Goes Here"));
 
             // Start a line count and declare a string to hold our current line.
             int linecount = 0;
-                String line;
+            String line;
 
             // Let the user know what we are searching for
             System.out.println("Searching for " + stringSearch + " in file...");
 
             // Loop through each line, stashing the line into our line variable.
-            while (( line = bf.readLine()) != null){
+            while ((line = bf.readLine()) != null) {
                 // Increment the count and find the index of the word
                 linecount++;
                 int indexfound = line.indexOf(stringSearch);
 
                 // If greater than -1, means we found the word
                 if (indexfound > -1) {
-                    System.out.println("Pattern Matched at Position " + indexfound + " on line text " + linecount);
+                    System.out.println("Pattern Matched at Position " + indexfound + " on line text " + linecount +
+                                       " is " + line);
                 }
             }
 
             // Close the file after done searching
             bf.close();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("IO Error Occurred: " + e.toString());
         }
     }
